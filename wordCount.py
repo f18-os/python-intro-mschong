8acb60dbeda3c0dc8b97e952635102234f04c3c6
mschong <marinachong96@gmail.com>
2018-09-02 01:04:25 -0600

    Finished. Need to fix small bug

--- wordCount.py
+++ wordCount.py
@@ -1,2 +1,24 @@
 import sys       
 import re   
+
+input = sys.argv[1]
+output = sys.argv[2]
+dict = {}
+list = []
+
+with open(input, 'r') as inputF:
+	for line in inputF:
+		line = line.strip()
+		words = re.split('[`\-=~!@#$%^&*()_+\[\]{};\'\\:"|<,./<>?\n\t ]+', line)
+		for item in words:
+			if item.lower() not in dict:
+				dict[item.lower()] = 1
+				list.append(item.lower())
+			else:
+				dict[item.lower()] += 1
+list.sort()
+
+outputF = open(output, 'w')
+for item in list:
+	outputF.write(item + " " + str(dict[item]) + "\n")
+outputF.close()

4f475a947acb66f0d4c31bd74cd6c79cd1de3f15
student <marinachong96@gmail.com>
2018-09-09 23:58:28 -0600

    redirecting works

--- shell/shell.py
+++ shell/shell.py
@@ -7,30 +7,32 @@ pid = os.getpid()               # get and remember pid
 os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
 
 rc = os.fork()
-args = sys.argv[0].split(" ")
+argss = sys.argv[1].split(" ")
+for arg in argss:
+    print(arg)
 if rc < 0:
     os.write(2, ("fork failed, returning %d\n" % rc).encode())
     sys.exit(1)
 
 elif rc == 0:                   # child
-    if args[1] == '>':
+    if argss[1] == '>':
         os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
                      (os.getpid(), pid)).encode())
 
         os.close(1)                 # redirect child's stdout
-        sys.stdout = open(args[2], "w")
+        sys.stdout = open(argss[2], "w")
         fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
         os.set_inheritable(fd, True)
         os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
 
         for dir in re.split(":", os.environ['PATH']): # try each directory in path
-            program = "%s/%s" % (dir, args[0])
+            program = "%s/%s" % (dir, argss[0])
             try:
-                os.execve(program, args, os.environ) # try to exec program
+                os.execve(program, argss, os.environ) # try to exec program
             except FileNotFoundError:             # ...expected
                 pass                              # ...fail quietly 
 
-        os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+        os.write(2, ("Child:    Error: Could not exec %s\n" % argss[0]).encode())
         sys.exit(1)                 # terminate with error
 
 

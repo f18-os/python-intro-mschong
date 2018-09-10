6b848c6a60f8e200c51bc71564e3ae8b52c27661
mschong <marinachong96@gmail.com>
2018-09-09 23:34:36 -0600

    trying stuff

--- shell/shell.py
+++ shell/shell.py
@@ -13,25 +13,28 @@ if rc < 0:
     sys.exit(1)
 
 elif rc == 0:                   # child
-    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-                 (os.getpid(), pid)).encode())
-    args = ["wc", "p3-exec.py"]
+    if sys.argv[1] == '>':
+        os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+                     (os.getpid(), pid)).encode())
 
-    os.close(1)                 # redirect child's stdout
-    sys.stdout = open("p4-output.txt", "w")
-    fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
-    os.set_inheritable(fd, True)
-    os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+        os.close(1)                 # redirect child's stdout
+        sys.stdout = open(sys.argv[2], "w")
+        fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+        os.set_inheritable(fd, True)
+        os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
 
-    for dir in re.split(":", os.environ['PATH']): # try each directory in path
-        program = "%s/%s" % (dir, args[0])
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly 
+        for dir in re.split(":", os.environ['PATH']): # try each directory in path
+            program = "%s/%s" % (dir, sys.argv[0])
+            try:
+                os.execve(program, sys.argv, os.environ) # try to exec program
+            except FileNotFoundError:             # ...expected
+                pass                              # ...fail quietly 
 
-    os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-    sys.exit(1)                 # terminate with error
+        os.write(2, ("Child:    Error: Could not exec %s\n" % sys.argv[0]).encode())
+        sys.exit(1)                 # terminate with error
+
+
+        
 
 else:                           # parent (forked ok)
     os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
